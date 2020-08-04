02761999135ca4ae893d9214d4e32a13826058b9
jshafto <jpshafto@gmail.com>
2020-08-04 12:03:11 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Binary Trees Project
+Completed as part of the App Academy curriculum. Implementation by Juliet Shafto and Ammar Nassri.
+
+## Project Description
+This project contains skeletons and tests to implement a generic binary tree, and a binary search tree.
+
+## Project Status
+- Binary Tree
+    - [ ] Binary tree implementation
+    - [ ] Bonus: build a tree from traversal
+- Binary Search Tree
+    - [ ] Binary search tree implementation
+    - [ ] Bonus: build a balanced BST from a sorted list
+    - [ ] Bonus: determine if a tree is balanced
---
+++ data-structures-binary-tree-starter/lib/leet_code_105.js
@@ -0,0 +1,25 @@
+// View the full problem and run the test cases at:
+//  https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
+
+// Given preorder and inorder traversal of a tree,
+// construct the binary tree.
+
+// Note:
+// You may assume that duplicates do not exist in the tree.
+
+// For example, given
+
+// preorder = [3,9,20,15,7]
+// inorder = [9,3,15,20,7]
+// Return the following binary tree:
+// 3
+// / \
+// 9  20
+//  /  \
+// 15   7
+const { TreeNode } = require('./tree_node.js');
+
+
+function buildTree(preorder, inorder) {
+
+}
